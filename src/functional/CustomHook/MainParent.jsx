5d3f463e6a8a3756c155c7daf29f4a4c0c983ea1
jasleenkaur1014 <jasleenkaur1014@gmail.com>
2026-05-23 19:11:26 +0530

    Removed state management logic from parent...

--- src/functional/CustomHook/MainParent.jsx
+++ src/functional/CustomHook/MainParent.jsx
@@ -1,73 +1,36 @@
-import { useEffect, useState } from "react";
 import Navbar from "./pages/Navbar";
 import Sidebar from "./pages/Sidebar";
-import useFetch from "./hooks/useFetch";
 import MainContent from "./pages/MainContent";
 import styled from "styled-components";
 import Cart from "./modals/Cart";
 import Liked from "./modals/Liked";
 import Saved from "./modals/Saved";
+import CustomAppContext, { useAppContext } from "./appContext";
 
 export default function CustomProjectParent() {
-  const [openedModal, setOpenedModal] = useState("");
-  const [search, setSearch] = useState("");
-  const [select, setSelect] = useState("products");
-  const [page, setPage] = useState(1);
-  const [cartItems, setCartItems] = useState([]);
-  const [likedPosts, setLikedPosts] = useState([]);
-  const [savedRecipes, setSavedRecipes] = useState([]);
-  const [like, setlike] = useState(false);
-  const { data, loading } = useFetch(select, page, search);
+  return (
+    <CustomAppContext>
+      <App />
+    </CustomAppContext>
+  );
+}
 
-  useEffect(() => {
-    setPage(1);
-  }, [search]);
+function App() {
+  const { openedModal } = useAppContext();
+
   return (
     <>
-      <Navbar
-        setOpenedModal={setOpenedModal}
-        setSearch={setSearch}
-        search={search}
-      />
+      <Navbar />
       <Container>
-        <Sidebar setSelect={setSelect} select={select} />
+        <Sidebar />
         <MainContentWrapper>
-          <MainContent
-            select={select}
-            loading={loading}
-            data={data}
-            cartItems={cartItems}
-            setCartItems={setCartItems}
-            page={page}
-            setPage={setPage}
-            likedPosts={likedPosts}
-            setLikedPosts={setLikedPosts}
-            savedRecipes={savedRecipes}
-            setSavedRecipes={setSavedRecipes}
-          />
+          <MainContent />
         </MainContentWrapper>
       </Container>
-      {openedModal == "cart" && (
-        <Cart
-          setOpenedModal={setOpenedModal}
-          cartItems={cartItems}
-          setCartItems={setCartItems}
-        />
-      )}
-      {openedModal == "liked" && (
-        <Liked
-          setOpenedModal={setOpenedModal}
-          likedPosts={likedPosts}
-          setLikedPosts={setLikedPosts}
-        />
-      )}
-      {openedModal == "saved" && (
-        <Saved
-          setOpenedModal={setOpenedModal}
-          savedRecipes={savedRecipes}
-          setSavedRecipes={setSavedRecipes}
-        />
-      )}
+
+      {openedModal === "cart" && <Cart />}
+      {openedModal === "liked" && <Liked />}
+      {openedModal === "saved" && <Saved />}
     </>
   );
 }
