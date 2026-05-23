99aaa51f1fb10947ad27ef73375de2c6e0b2950a
jasleenkaur1014 <jasleenkaur1014@gmail.com>
2026-05-23 19:08:45 +0530

    Refactor MainContent to use custom context to solve prop drilling

--- src/functional/CustomHook/pages/MainContent.jsx
+++ src/functional/CustomHook/pages/MainContent.jsx
@@ -1,60 +1,18 @@
+import { useAppContext } from "../appContext";
 import Posts from "./Posts";
 import Products from "./Products";
 import Recipes from "./Recipes";
 import Users from "./Users";
 
 export default function MainContent(props) {
-  const {
-    select,
-    setCartItems,
-    cartItems,
-    addedCart,
-    setAddedCart,
-    page,
-    setPage,
-    data,
-    loading,
-    likedPosts,
-    setLikedPosts,
-    savedRecipes,
-    setSavedRecipes,
-  } = props;
+  const { select } = useAppContext();
 
   return (
     <>
-      {select === "products" && (
-        <Products
-          data={data}
-          loading={loading}
-          setCartItems={setCartItems}
-          cartItems={cartItems}
-          page={page}
-          setPage={setPage}
-        />
-      )}
-      {select === "users" && (
-        <Users data={data} loading={loading} page={page} setPage={setPage} />
-      )}
-      {select === "recipes" && (
-        <Recipes
-          data={data}
-          loading={loading}
-          page={page}
-          setPage={setPage}
-          savedRecipes={savedRecipes}
-          setSavedRecipes={setSavedRecipes}
-        />
-      )}
-      {select === "posts" && (
-        <Posts
-          data={data}
-          loading={loading}
-          page={page}
-          setPage={setPage}
-          likedPosts={likedPosts}
-          setLikedPosts={setLikedPosts}
-        />
-      )}
+      {select === "products" && <Products />}
+      {select === "users" && <Users />}
+      {select === "recipes" && <Recipes />}
+      {select === "posts" && <Posts />}
     </>
   );
 }
