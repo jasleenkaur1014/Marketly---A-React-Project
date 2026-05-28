727c13af1a4278ee271004409ab021dd3e293aeb
jasleenkaur1014 <jasleenkaur1014@gmail.com>
2026-05-28 20:30:34 +0530

    Implement routing with react-router-dom

--- src/functional/CustomHook/MainParent.jsx
+++ src/functional/CustomHook/MainParent.jsx
@@ -1,21 +1,45 @@
 import Navbar from "./pages/Navbar";
 import Sidebar from "./pages/Sidebar";
-import MainContent from "./pages/MainContent";
 import styled from "styled-components";
 import Cart from "./modals/Cart";
 import Liked from "./modals/Liked";
 import Saved from "./modals/Saved";
 import CustomAppContext, { useAppContext } from "./appContext";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Products from "./pages/Products";
+import Posts from "./pages/Posts";
+import Users from "./pages/Users";
+import Recipes from "./pages/Recipes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="/products" /> },
+
+      { path: "products", element: <Products /> },
+      { path: "posts", element: <Posts /> },
+      { path: "users", element: <Users /> },
+      { path: "recipes", element: <Recipes /> },
+    ],
+  },
+]);
 
 export default function CustomProjectParent() {
   return (
     <CustomAppContext>
-      <App />
+      <RouterProvider router={router} />
     </CustomAppContext>
   );
 }
 
-function App() {
+function Layout() {
   const { openedModal } = useAppContext();
 
   return (
@@ -24,7 +48,7 @@ function App() {
       <Container>
         <Sidebar />
         <MainContentWrapper>
-          <MainContent />
+          <Outlet />
         </MainContentWrapper>
       </Container>
 
